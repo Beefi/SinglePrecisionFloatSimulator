716e1b066f968a1ee49eb6ceeb18dd2d4764a26b
Miguel Baliog <miguelbaliog@gmail.com>
2022-02-10 22:40:07 +0800

    Changed to Decimal32

--- src/HexInputForm.java
+++ src/HexInputForm.java
@@ -3,6 +3,8 @@ import org.firebirdsql.decimal.Decimal32;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class HexInputForm extends JFrame {
     private JTextField hexTextField;
@@ -29,13 +31,32 @@ public class HexInputForm extends JFrame {
                 String hexInput = hexTextField.getText();
                 String fixedPoint = fixedSpinner.getValue().toString();
 
-                Decimal32 Output = controller.ComputeHex(hexInput);
-                String strFormat = String.format("%."+fixedPoint+"f", Output);
-                outputField.setText(strFormat);
+                Long intBits = Long.parseLong(hexInput, 16);
+                float floatOutput = Float.intBitsToFloat(intBits.intValue());
 
-                if (printBox.isSelected()) {
-                    StringBuilder sb = new StringBuilder("0x" + hexInput + " : " + strFormat);
-                    controller.PrintToText(sb);
+                if (!Double.isFinite(floatOutput)) {
+                    outputField.setText(String.valueOf(floatOutput));
+
+                    if (printBox.isSelected()) {
+                        StringBuilder sb = new StringBuilder(hexInput + " : " + floatOutput);
+                        controller.PrintToText(sb);
+                    }
+                }
+
+                else {
+                    float Output = controller.ComputeHex(hexInput);
+                    BigDecimal bd = BigDecimal.valueOf(Output);
+
+                    bd = bd.movePointLeft(Integer.parseInt(fixedPoint));
+                    //bd = bd.scaleByPowerOfTen(Integer.parseInt(fixedPoint));
+                    String strFormat = bd.toString();
+
+                    outputField.setText(strFormat);
+
+                    if (printBox.isSelected()) {
+                        StringBuilder sb = new StringBuilder("0x" + hexInput + " : " + strFormat);
+                        controller.PrintToText(sb);
+                    }
                 }
             }
         });
@@ -45,12 +66,14 @@ public class HexInputForm extends JFrame {
                 Controller controller = new Controller();
 
                 String hexInput = hexTextField.getText();
-                Decimal32 Output = controller.ComputeHex(hexInput);
+                float Output = controller.ComputeHex(hexInput);
 
-                outputField.setText(String.valueOf(Output));
+                String finalFormat = String.valueOf(Output);
+
+                outputField.setText(finalFormat);
 
                 if (printBox.isSelected()) {
-                    StringBuilder sb = new StringBuilder("0x" + hexInput + " : " + Output);
+                    StringBuilder sb = new StringBuilder("0x" + hexInput + " : " + finalFormat);
                     controller.PrintToText(sb);
                 }
             }
